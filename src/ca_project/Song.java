d52e250ce7c0ea9b37641b0dbbbc18f9ca56cb4e
Danielggz <dggz1993@gmail.com>
2024-03-12 09:49:28 +0000

    First commit. Add classes and interfaces

---
+++ src/ca_project/Song.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package ca_project;
+
+/**
+ * @author Daniel García
+ * 12/03/2024
+ */
+public class Song {
+
+}
---
+++ src/ca_project/StackInterface.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+package ca_project;
+
+/**
+ *
+ * @author Daniel García
+ * 12/03/2024
+ */
+public interface StackInterface {
+
+}
